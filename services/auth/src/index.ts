2d142dc02432678e1e48d8d3f6214a7f91eb2421
sifat1234 <ahahmedsifat111@gmail.com>
2026-02-19 16:32:45 +0600

    feat(db): implement MongoDB connection setup

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import dovenv from 'dotenv';
+import connectDB from './config/db.js';
 dovenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Auth service is running on port ${PORT}`);
 });
